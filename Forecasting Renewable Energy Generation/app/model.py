499d058c312d25cc51d008b74000bf21ba90c237
Tabjulu Ram Sheshu <tabjuluram@gmail.com>
2025-05-12 17:12:21 +0530

    flask app edit 1

--- Forecasting Renewable Energy Generation/app/model.py
+++ Forecasting Renewable Energy Generation/app/model.py
@@ -3,10 +3,9 @@ import pandas as pd
 import xgboost as xgb
 import pickle
 
-base = '/Users/adityavs14/Documents/Internship/Pianalytix/Renew_Ener_Pred/ML_Model/app'
+base = '/Users/ramta/Documents/GitHub/Data-Science-Projects/Forecasting Renewable Energy Generation/app'
 reg = xgb.XGBRegressor()
 model = pickle.load(open(f'{base}/model2.pkl', "rb"))
-#model = reg.load_model(f'{base}/model2.json')
 
 def create_f(df):
     df = df.copy()
@@ -20,7 +19,6 @@ def create_f(df):
     df['weekofyear'] = df.index.isocalendar().week
     
     return df
-
 
 def create_pd(fr,to):
     new = pd.date_range(fr+' 12:00:00+00:00',to+' 12:00:00+00:00', freq='10min')
